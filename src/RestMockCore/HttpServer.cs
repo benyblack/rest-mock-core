fefd096f3956e14b5284defdb75298a58d3dbbd1
Behnam <benyblack@gmail.com>
2022-02-23 08:13:43 +0100

    Refactor httpserver
    Extract a method for handling when the route
    does not have any math in the routing table.

--- src/RestMockCore/HttpServer.cs
+++ src/RestMockCore/HttpServer.cs
@@ -1,70 +1,71 @@
-﻿using System.Linq;
-using System.Text;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using RestMockCore.Interfaces;
 using RestMockCore.Models;
 
-namespace RestMockCore
+namespace RestMockCore;
+public class HttpServer : IHttpServer
 {
-    public class HttpServer : IHttpServer
+    private IWebHost _host;
+    private readonly string _hostname;
+    private readonly int _port;
+
+    public IRequestBuilder Config { get; set; }
+
+    public HttpServer(int port = 5000, string hostname = "localhost")
     {
-        private IWebHost _host;
-        private readonly string _hostname;
-        private readonly int _port;
+        _hostname = hostname;
+        _port = port;
+        Config = new RequestBuilder();
+    }
 
-        public IRequestBuilder Config { get; set; }
-
-        public HttpServer(int port = 5000, string hostname = "localhost")
-        {
-            _hostname = hostname;
-            _port = port;
-            Config = new RequestBuilder();
-        }
-
-        public void Run()
-        {
-            _host = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls($"http://{_hostname}:{_port}")
-                .Configure(app =>
+    public void Run()
+    {
+        _host = new WebHostBuilder()
+            .UseKestrel()
+            .UseUrls($"http://{_hostname}:{_port}")
+            .Configure(app =>
+            {
+                app.Run(async context =>
                 {
-                    app.Run(async context =>
+                    var route = Config.RouteTable?.LastOrDefault(x => x.IsMatch(context.Request));
+
+                    if (route == null)
                     {
-                        var route = Config.RouteTable?.LastOrDefault(x => x.IsMatch(context.Request));
-                        
-                        switch (route)
-                        {
-                            case null when context.Request.Path == @"/":
-                                context.Response.ContentType = "text/plain";
-                                await context.Response.WriteAsync("It Works!", Encoding.UTF8);
-                                return;
-                            case null:
-                                context.Response.StatusCode = StatusCodes.Status404NotFound;
-                                context.Response.ContentType = "text/plain";
-                                await context.Response.WriteAsync("Page not found!", Encoding.UTF8);
-                                return;
-                        }
+                        HandleRouteNotFound(context);
+                        return;
+                    }
 
-                        if (route.Response.Handler != null)
-                        {
-                            route.Response.Handler(context);
-                            return;
-                        }
+                    if (route.Response.Handler != null)
+                    {
+                        route.Response.Handler(context);
+                        return;
+                    }
 
-                        var response = route.Response.Body;
-                        context.Response.StatusCode = route.Response.StatusCode;
-                        context.Response.Headers.AddRange(route.Response.Headers);
-                        await context.Response.WriteAsync(response, Encoding.UTF8).ConfigureAwait(false);
-                    });
-                }).Build();
-            _host.Start();
-        }
+                    var responseText = route.Response.Body;
+                    context.Response.StatusCode = route.Response.StatusCode;
+                    context.Response.Headers.AddRange(route.Response.Headers);
+                    await context.Response.WriteAsync(responseText, Encoding.UTF8).ConfigureAwait(false);
+                });
+            }).Build();
+        _host.Start();
+    }
 
-        public void Dispose()
+    private async void HandleRouteNotFound(HttpContext context)
+    {
+        if (context.Request.Path == @"/")
         {
-            _host?.Dispose();
+            context.Response.ContentType = "text/plain";
+            await context.Response.WriteAsync("It Works!", Encoding.UTF8);
+            return;
         }
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Page not found!", Encoding.UTF8);
+    }
+
+    public void Dispose()
+    {
+        _host?.Dispose();
     }
 }
